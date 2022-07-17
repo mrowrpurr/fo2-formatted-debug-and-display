e5beb89a2a8a84d9e57d2de3369348ea928c09b4
Mrowr Purr <87039589+mrowrpurr@users.noreply.github.com>
2022-07-17 12:36:58 -0700

    Add usage comment

--- scripts/headers/FormattedDebug.h
+++ scripts/headers/FormattedDebug.h
@@ -1,3 +1,29 @@
+/*
+    Usage:
+
+        #define DEBUG_NAME "MyMod"
+        
+        procedure start begin
+            // Prints to debug.log (depending on ddraw.ini configured DebugMode)
+            // as "[MyMod] hello"
+            debug("hello");
+            
+            variable x = 69;
+            variable y = 420;
+            
+            // Print out a variable value
+            debug1("hello", x); // <--- prints "[MyMod] hello (69)"
+            
+            // Print out multiple variable values
+            debug1("hello", x, y); // <--- prints "[MyMod] hello (69) (420)"
+            
+            // Or specify where in the text the value(s) should go using "%s" as a placeholder
+            // Must use the debug*f functions. If you use "%s" with the normal functions, Fallout will crash.
+            debug1f("hello %s world", x);    // <--- prints "[MyMod] hello 69 world"
+            debug2f("hi %s there %s", x, y); // <--- prints "[MyMod] hi 69 there 420"
+        end
+*/
+
 #include "sfall/lib.strings.h"
 #include "sfall/lib.arrays.h"
 
